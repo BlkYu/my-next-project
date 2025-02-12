ccf59e285ea746ffdf6bdb45cf3a5bb3467b9226
YU <ankoanko3.14@gmail.com>
2025-02-12 14:48:31 +0900

    3-3-1.データを表示させよう

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,61 @@
 import styles from "./page.module.css";
 import Image from "next/image"
 
+type News = {
+  id: string;
+  title: string;
+  category:{
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+}
+
+const data:{contents: News[]} = {
+  contents: [
+    {
+    id: "1",
+    title: "渋谷にオフィスを移転しました",
+    category:{
+      name: "更新情報",
+    },
+    publishedAt: "2023/05/19",
+    createdAt: "2023/05/19",
+    },
+    {
+    id: "2",
+    title: "当社CEOが業界リーダTOP30に選出されました",
+    category:{
+      name: "更新情報",
+    },
+    publishedAt: "2023/05/19",
+    createdAt: "2023/05/19",
+    },
+    {
+    id: "3",
+    title: "テストの記事です",
+    category:{
+      name: "更新情報",
+    },
+    publishedAt: "2023/04/19",
+    createdAt: "2023/04/19",
+    },
+]}
+
 export default function Home() {
   //JavaScriptの定数や式などを書くことができる
   const name = "世界";
 
+  //件数を2件に絞り込む
+  const sliceData = data.contents.slice(0, 2)
+
   //HTMLのようなマークアップを書くことができる
   //JavaScriptを書いたり、参照する際には{}で囲む
   return (
+    <>
     <section className={styles.top}>
       <div>
-        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
+        <h1 className={styles.title}>ふじ珠算教室</h1>
         <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
       </div>
       <Image 
@@ -21,5 +66,39 @@ export default function Home() {
         height={1200}
         />
     </section>
+    <section className={styles.news}>
+      <h2 className={styles.newsTitle}>News</h2>
+      <ul>
+        {sliceData.map((article) => (
+          <li key={article.id} className={styles.list}>
+            <div className={styles.link}>
+              <Image 
+              className={styles.image}
+              src="/no-image.png"
+              alt="No Image"
+              width={1200}
+              height={630}
+              />
+              <dl className={styles.content}>
+                <dt className={styles.newsItemTitle}>{article.title}</dt>
+                <dd className={styles.meta}>
+                  <span className={styles.tag}>{article.category.name}</span>
+                  <span className={styles.date}>
+                    <Image 
+                    src="/clock.svg"
+                    alt=""
+                    width={16}
+                    height={16}
+                    priority/>
+                    {article.publishedAt}
+                  </span>
+                </dd>
+              </dl>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+    </>
   );
 }
