8148e6353d07ca487821a555f9bdc32cd95111ab
YU <ankoanko3.14@gmail.com>
2025-02-27 21:20:46 +0900

    7-2-4.現在のページ番号がわかるようにしよう

--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -1,5 +1,8 @@
+import { notFound } from "next/navigation";
 import NewsList from "@/app/_components/NewsList";
 import { getNewsList } from "@/app/_libs/microcms";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import Pagination from "@/app/_components/Pagination";
 
 type Props = {
     params: {
@@ -9,10 +12,24 @@ type Props = {
 
 export default async function Page({params}: Props){
     const current = parseInt(params.current, 10);
-    const {contents: news} = await getNewsList({
-        limit: 10,
-        offset: 10 * (current- 1)
+    
+    if(Number.isNaN(current)|| current < 1){
+        notFound();
+    }
+
+    const {contents: news, totalCount} = await getNewsList({
+        limit: NEWS_LIST_LIMIT,
+        offset: NEWS_LIST_LIMIT * (current- 1)
     });
 
-    return <NewsList news={news}/>
+    if(news.length==0){
+        notFound();
+    }
+    
+    return (
+        <>
+            <NewsList news={news}/>
+            <Pagination totalCount={totalCount} current={current}/>
+        </>
+    )
 }
