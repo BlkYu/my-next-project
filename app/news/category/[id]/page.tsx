3df084800333c2e9ccb2beb00f7536bf663501ed
YU <ankoanko3.14@gmail.com>
2025-02-28 11:51:27 +0900

    7-4-2.検索機能を実装しよう

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -3,17 +3,19 @@ import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
 import { notFound } from "next/navigation";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import Pagination from "@/app/_components/Pagination";
 
 type Props = {
     params: {
         id: string;
+        current: string;
     }
 }
 
 export default async function Page({params }: Props){
     const category = await getCategoryDetail(params.id).catch(notFound);
 
-    const{contents: news} = await getNewsList({
+    const{contents: news, totalCount} = await getNewsList({
         limit: NEWS_LIST_LIMIT,
         filters: `category[equals]${category.id}`,
     })
@@ -24,6 +26,9 @@ export default async function Page({params }: Props){
                 <Category category={category}/> の一覧
             </p>
             <NewsList news={news}/>
+            <Pagination 
+            totalCount={totalCount}
+            basePath={`/news/category/${category.id}`}/>
         </>
     )
 }
