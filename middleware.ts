a82150dc08d2cbd60d4a03be304e245d5e93d54b
YU <ankoanko3.14@gmail.com>
2025-03-06 19:11:33 +0900

    8-6-4.Next.jsでBasic認証を設定しよう

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
 
+export const middleware = createNextAuthMiddleware();
 
-export function middleware (request: NextRequest) {
-    console.log("middleware: " + request.url);
-
-    return NextResponse.next();
+export const config = {
+    matcher: ["/(.*)"],
 }
-
-export const config = {};
