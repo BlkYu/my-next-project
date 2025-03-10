85651485071e093e59c40e1367e45a5cc94ca26a
YU <ankoanko3.14@gmail.com>
2025-03-10 18:33:05 +0900

    10-1.メタデータを設定しよう/メンバーページ、問い合わせページの設定

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,6 +1,10 @@
 import Hero from "../_components/Hero";
 import Sheet from "@/app/_components/Sheet"
 
+export const metadata = {
+    title: "お問い合わせ",
+};
+
 type Props = {
     children: React.ReactNode;
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,24 @@
+import { Metadata } from "next";
 import Footer from "./_components/Footer";
 import Header from "./_components/Header";
 import "./globals.css";
+
+export const metadata:Metadata = {
+  metadataBase: new URL("https://localhost:3000"),
+  title: {
+    template: "%s | シンプルなコーポレートサイト",
+    default: "シンプルなコーポレートサイト",
+  },
+  description: "「Next.js+ヘッドレスCMSで始める！簡単・モダンWebサイト制作入門」で作成されるサイトです。",
+  openGraph: {
+    title: "シンプルなコーポレートサイト",
+    description: "「Next.js+ヘッドレスCMSで始める！簡単・モダンWebサイト制作入門」で作成されるサイトです。",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    canonical: "https://localhost:3000",
+  },
+}
 
 export default function RootLayout({
   children,
